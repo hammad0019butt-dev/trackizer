c7903efa8d6481b07e84a4309db30d940322e529
hammad0019butt-dev <hammad0019butt@gmail.com>
2026-04-27 16:16:59 +0500

    .

--- lib/view/login/social login.dart
+++ lib/view/login/social login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:trackizer/common/color_extention.dart';
 import 'package:trackizer/common_widgets/button/my_button.dart';
